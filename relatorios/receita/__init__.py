e07e2688dadc65c73b186ac7c5e268712f20075f
marciomartinho <marciomartinho@gmail.com>
2025-07-05 16:20:25 -0300

    atualizar

--- relatorios/receita/__init__.py
+++ relatorios/receita/__init__.py
@@ -8,7 +8,7 @@ from .receita_estimada import gerar_relatorio_receita_estimada
 from .receita_por_adm import gerar_relatorio_por_adm
 from .receita_atualizada import gerar_relatorio_receita_atualizada_vs_inicial
 from .grafico_pizza import gerar_grafico_receita_liquida
-from .receita_conta_corrente import gerar_relatorio_receita_conta_corrente
+from .receita_conta_corrente import gerar_relatorio_receita_conta_corrente  # ATUALIZADO
 
 # Aliases para compatibilidade
 from .receita_estimada import gerar_relatorio_receita_estimada as gerar_relatorio_estimada
@@ -19,6 +19,6 @@ __all__ = [
     'gerar_relatorio_por_adm', 
     'gerar_relatorio_receita_atualizada_vs_inicial',
     'gerar_grafico_receita_liquida',
-    'gerar_relatorio_receita_conta_corrente',
+    'gerar_relatorio_receita_conta_corrente',  # ATUALIZADO
     'gerar_relatorio_estimada'  # Alias para compatibilidade
 ]
